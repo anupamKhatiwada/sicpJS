ec87a2806f72ec0263c562814dc68c0d14ed9c55
anupamKhatiwada <67192883+anupamKhatiwada@users.noreply.github.com>
2021-12-26 02:25:45 +0530

    Update genericAdder.js

--- genericAdder.js
+++ genericAdder.js
@@ -1,3 +1,21 @@
+/*
+
+    Below are three adder functions that add values strating from one number and end after reaching a larger number.
+    The way they add values are different and those are quite self evident upon looking at the functions.
+    Most part of the functions are same. The only part that is different is the actual application procedure of adding the values.
+    The general pattern of recursion is the same for all three functions.
+    
+    
+    Our objective is to write a generic adder function which takes replacable add functions so that we can abstract out the recursive
+    part. 
+    
+
+
+*/
+
+
+
+
 function cube(x) {
     return x * x * x;
 }
@@ -8,7 +26,7 @@ function sum_cubes(a, b) {
            : cube(a) + sum_cubes(a + 1, b);
 }
 
-// sum_cubes(3, 7);
+
 
 
 function sum_integers(a, b) {
@@ -21,9 +39,41 @@ function sum_integers(a, b) {
 
 
 function pi_sum(a, b) {
+    
+    
+    let relation  = (a) => 1 / (a * (a + 2));
+    let incrementer = (a) => a+4;
+    
+    
+    
+    return genericAdder(a,b,relation,incrementer);
+}
+
+
+
+
+
+
+function genericAdder(a,b,additionRelation,incrementer){
     return a > b
            ? 0
-           : 1 / (a * (a + 2)) + pi_sum(a + 4, b);
+           : additionRelation(a,b) + genericAdder(incrementer(a,b), b);
+    /*
+        
+        We are passing both the available parameters while calling the inner functions because we di
+    
+    
+    */
+    
+    
+    
 }
+
+
+
+
+
+
+
 
 
